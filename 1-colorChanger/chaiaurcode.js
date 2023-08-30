0dda72d13d2c67fe22c524896ee8c6e883603baa
harshnishad <91336535+harshnishad@users.noreply.github.com>
2023-08-30 23:06:53 +0530

    Initial commit

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll('.button');
+
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    if (e.target.id == 'grey') {
+      body.style.backgroundColor = 'grey';
+    } else if (e.target.id == 'blue') {
+      body.style.backgroundColor = 'blue';
+    } else if (e.target.id == 'yellow') {
+      body.style.backgroundColor = 'yellow';
+    } else {
+      body.style.backgroundColor = 'white';
+    }
+  });
+});
